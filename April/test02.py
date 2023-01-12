22c45773d64ba983f56490990e7c46a8e19e2bff
team 2984 <team2984Git@gmail.com>
2023-01-11 17:21:49 -0800

    Initial Raspberry Pi code backup to GitHub. Includes basic apriltag discovery.

--- April/test02.py
+++ April/test02.py
@@ -1,13 +1,19 @@
 import cv2
+<<<<<<< HEAD
 import math
+=======
+>>>>>>> Initial Raspberry Pi code backup to GitHub. Includes basic apriltag discovery.
 import numpy as np
 import apriltag
 import imutils
 from apriltag import Detector
 capture = cv2.VideoCapture('/dev/video1', cv2.CAP_V4L)
+<<<<<<< HEAD
 
 data_log = 'homography' #homography, id, decision margin, center
 
+=======
+>>>>>>> Initial Raspberry Pi code backup to GitHub. Includes basic apriltag discovery.
 while True:
     ret, frame = capture.read()
     cv2.imshow("Output", frame)
@@ -19,6 +25,7 @@ while True:
     detector = apriltag.Detector()
     detector.add_tag_family("tag16h5")
     detections = detector.detect(image)
+<<<<<<< HEAD
     #if detections:
         # print(detections[0])
         #homography = detections[0].homography
@@ -30,4 +37,6 @@ while True:
         #fdistance_to = str(math.sqrt(hx * hx + hy * hy + hz * hz))
         #print(fhomography)
         #print(f"Family: {str(detections[0].tag_family)}, Tag: {str(detections[0].tag_id)}, Distance of {str(detections[0].homography)} to center of {str(detections[0].center)}")
+=======
+>>>>>>> Initial Raspberry Pi code backup to GitHub. Includes basic apriltag discovery.
     print(detections)
