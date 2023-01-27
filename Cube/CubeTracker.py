17a692559fbe8eb27319f2ee404eafce8869d4b7
SchooComp2984 <team2984Git@gmail.com>
2023-01-26 16:53:11 -0800

    cubetracker

--- Cube/CubeTracker.py
+++ Cube/CubeTracker.py
@@ -1,7 +1,6 @@
 # Import code lib for various commands used in the code
 from collections import deque
 from imutils.video import VideoStream
-from networktables import NetworkTables
 import numpy as np
 import cv2
 import imutils
@@ -12,7 +11,7 @@ hsv_green = cv2.cvtColor(greenBGR,cv2.COLOR_BGR2HSV)
 print (hsv_green)
 # Begin writing varibles that do not change throughout varibles
 
-capture = cv2.VideoCapture('/dev/video1', cv2.CAP_V4L)
+capture = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
 
 
 # Begin repeated capture of video
@@ -33,10 +32,27 @@ while True:
     #upper_mask = cv2.inRange(frame, lower2, upper2)
      
     #full_mask = lower_mask + upper_mask;
-    full_mask = cv2.inRange(frame, (120, 50, 20), (140, 150, 255))
+    kernel = np.ones((5, 5), np.uint8)
+    full_mask = cv2.inRange(frame, (110, 80, 0), (130, 255, 255))
     result = cv2.bitwise_and(result, result, mask=full_mask)
+    eroded_mask = cv2.erode(full_mask, kernel, cv2.BORDER_REFLECT)
+    h, s, v = frame[:, :, 0], frame[:, :, 1], frame[:, :, 2]
+    #image = cv2.circle(image, (x,y), radius=0, color=(0, 0, 255), thickness=-1)
+    # cv2.imshow("masked", masked_img)
+    ret,thresh = cv2.threshold(full_mask,127,255,0)
      
-    cv2.imshow('mask', full_mask)
-    cv2.imshow('result', result)
-    cv2.imshow("Capture", frame)
+    # calculate moments of binary image
+    M = cv2.moments(thresh)
+    
+    # calculate x,y coordinate of center
+    cX = int(M["m10"] / M["m00"])
+    cY = int(M["m01"] / M["m00"])
+    
+    # put text and highlight the center
+    cv2.circle(eroded_mask, (cX, cY), 5, (155, 155, 155), -1)
+    cv2.putText(eroded_mask, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (155, 155, 155), 2)
+    
+    cv2.imshow('result', eroded_mask)
+    #cv2.imshow("Hue", h)
+    #cv2.imshow("Satuation", s)
     cv2.waitKey(1)
