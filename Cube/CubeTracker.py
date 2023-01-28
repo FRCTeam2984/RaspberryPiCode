c1f148746d63646a54c43528579788c714f68237
SchooComp2984 <team2984Git@gmail.com>
2023-01-27 16:44:05 -0800

    blob detections

--- Cube/CubeTracker.py
+++ Cube/CubeTracker.py
@@ -12,13 +12,14 @@ print (hsv_green)
 # Begin writing varibles that do not change throughout varibles
 
 capture = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
-
+capture.set(3, 640)
+capture.set(4, 480)
 
 # Begin repeated capture of video
 while True:
     ret, frame = capture.read()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    result = frame.copy()
+    frame_copy = frame.copy()
     # mask = cv2.inRange()
     # lower boundary RED color range values; Hue (0 - 10)
     #lower1 = np.array([0, 100, 20])
@@ -32,27 +33,61 @@ while True:
     #upper_mask = cv2.inRange(frame, lower2, upper2)
      
     #full_mask = lower_mask + upper_mask;
-    kernel = np.ones((5, 5), np.uint8)
-    full_mask = cv2.inRange(frame, (110, 80, 0), (130, 255, 255))
-    result = cv2.bitwise_and(result, result, mask=full_mask)
-    eroded_mask = cv2.erode(full_mask, kernel, cv2.BORDER_REFLECT)
-    h, s, v = frame[:, :, 0], frame[:, :, 1], frame[:, :, 2]
+    kernel = np.ones((2, 2), np.uint8)
+    mask = cv2.inRange(frame, (110, 80, 0), (130, 255, 255))
+    mask_result = cv2.bitwise_and(frame_copy, frame_copy, mask=mask)
+    eroded_result = cv2.erode(mask, kernel, cv2.BORDER_REFLECT)
+    inverted_result = cv2.bitwise_not(eroded_result)
+    #h, s, v = frame[:, :, 0], frame[:, :, 1], frame[:, :, 2]
     #image = cv2.circle(image, (x,y), radius=0, color=(0, 0, 255), thickness=-1)
-    # cv2.imshow("masked", masked_img)
-    ret,thresh = cv2.threshold(full_mask,127,255,0)
+    retval, threshold = cv2.threshold(inverted_result, 200, 255, cv2.THRESH_BINARY_INV)
+    params = cv2.SimpleBlobDetector_Params()
+
+    # Change thresholds
+    params.minThreshold = 10;
+    params.maxThreshold = 400;
      
+    # Filter by Area.
+    params.filterByArea = True
+    params.minArea = 1500
+     
+    # Filter by Circularity
+    params.filterByCircularity = False
+    params.minCircularity = 0.1
+     
+    # Filter by Convexity
+    params.filterByConvexity = False
+    params.minConvexity = 0.87
+     
+    # Filter by Inertia
+    params.filterByInertia = False
+    params.minInertiaRatio = 0.01
+
+    detector = cv2.SimpleBlobDetector_create(params)
+
+    # Set up the detector with default parameters.
+    #detector = cv2.SimpleBlobDetector()
+
+    # Detect blobs.
+    keypoints = detector.detect(inverted_result)
+    print(keypoints)
+    # Draw detected blobs as red circles.
+    # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+    im_with_keypoints = cv2.drawKeypoints(inverted_result, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    # Show keypoints
+    cv2.imshow("Keypoints", im_with_keypoints)
     # calculate moments of binary image
-    M = cv2.moments(thresh)
-    
+    # M = cv2.moments(thresh)
+    # print(M)
     # calculate x,y coordinate of center
-    cX = int(M["m10"] / M["m00"])
-    cY = int(M["m01"] / M["m00"])
+    #fix division by 0 error (IMPORTANT)
+    # cX = int(M["m10"] / M["m00"])
+    # cY = int(M["m01"] / M["m00"])
     
     # put text and highlight the center
-    cv2.circle(eroded_mask, (cX, cY), 5, (155, 155, 155), -1)
-    cv2.putText(eroded_mask, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (155, 155, 155), 2)
+    # cv2.circle(inverted_result, (cX, cY), 5, (155, 155, 155), -1)
+    # cv2.putText(inverted_result, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (155, 155, 155), 2)
     
-    cv2.imshow('result', eroded_mask)
-    #cv2.imshow("Hue", h)
-    #cv2.imshow("Satuation", s)
+    # cv2.imshow('result', inverted_result)
     cv2.waitKey(1)
