86f9338da7c36d7a974371544ec67bb26abc8b3a
SchooComp2984 <team2984Git@gmail.com>
2023-01-30 17:31:30 -0800

    non-working cone detection

--- CubeConeTracker.py
+++ CubeConeTracker.py
@@ -11,6 +11,7 @@ import math
 capture = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
 capture.set(3, 480)
 capture.set(4, 360)
+capture.set(15, -8)
 
 def find_cube_pos(frame):
     frame_copy = frame.copy()
@@ -54,10 +55,10 @@ def find_cube_pos(frame):
 def find_cone_pos(frame):
     frame_copy = frame.copy()
 
-    pos_of_rel_center = None
+    centroid = None
     
     kernel = np.ones((3, 3), np.uint8)
-    mask = cv2.inRange(frame, (10, 30, 50), (35, 255, 255))
+    mask = cv2.inRange(frame, (10, 160, 0), (30, 255, 255))
     mask_result = cv2.bitwise_and(frame_copy, frame_copy, mask=mask)
     eroded_result = cv2.erode(mask, kernel, cv2.BORDER_REFLECT)
     inverted_result = cv2.bitwise_not(eroded_result)
@@ -80,42 +81,57 @@ def find_cone_pos(frame):
         cy = int(M['m01']/M['m00'])
         cv2.drawContours(frame, [highest_cnt], -1, (255, 0, 0), 2)
         cv2.circle(frame, (cx, cy), 4, (255,0, 0), -1)
-        pos_of_rel_center = [-480/2 + cx, 360/2 - cy]
+        centroid = [cx, cy]
         x,y,w,h = cv2.boundingRect(highest_cnt)
         wh_ratio = w/h
+        center = (int(x + w/2), int(y + h/2))
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (255, 255, 255), 2)
+        cv2.circle(frame, center, 4, (255,255, 255), -1)
+        centroid = (cx, cy)
+        diff = (center[0]-centroid[0], center[1] - centroid[1])
+        diff = diff/np.linalg.norm(diff)
+        #print(diff)
+        angle = math.atan2(diff[1], diff[0])
+        angle_deg = angle / math.pi * 180
         # this ratio should be > 1 if the width is greater than the height and < 1 if height is greater than 1
         if wh_ratio > 1:
             #print('fallen down and pointing with 45 degrees of to the left or right of the camera')
-            left_vector = [math.sqrt(w * h), 0]
-            left_vector = left_vector/np.linalg.norm(left_vector)
-            cone_vector = [w,h]
-            cone_vector = cone_vector/np.linalg.norm(cone_vector)
-            dt_product = np.dot(left_vector, cone_vector)
-            dt_product = left_vector[0] * cone_vector[0] + left_vector[1] * cone_vector[1]
-            print(dt_product)
+            #left_vector = [math.sqrt(w * h), 0]
+            #left_vector = left_vector/np.linalg.norm(left_vector)
+            #cone_vector = [w,h]
+            #cone_vector = cone_vector/np.linalg.norm(cone_vector)
+            #dt_product = np.dot(left_vector, cone_vector)
+            #dt_product = left_vector[0] * cone_vector[0] + left_vector[1] * cone_vector[1]
+            #print(cx, cy, x, y)
+            #print(dt_product)
+            #angle = math.acos(diff[0])
+            pass
         else:
             #print('standing up or fallen down but within 45 degrees of yaw of being pointed away or at the camera')
-            up_vector = [[0, 0], [0, 1]]
-            cone_vector = [[w,h], [0, 0]]
+            #angle = math.atan2(diff[1], diff[0])
+            pass
+        cv2.line(frame, (cx, cy), center, (0,0, 255), 2)
+        #cv2.line(frame, (cx, cy), (int(cx + math.cos(angle) * 40), int(cy + math.sin(angle) * 40)), (0,0, 255), 2)
+        print(angle_deg)
             
             
     cv2.imshow("Cone Result", inverted_result)
     
-    if pos_of_rel_center == None:
+    if centroid == None:
         #print('Cube not on screen')
         return None
     else:
         #print(pos_of_rel_center)
         #sender.send_cone_data([True, pos_of_rel_center[0], pos_of_rel_center[1]])
-        return pos_of_rel_center
+        return centroid
 
 # Begin repeated capture of video
 while True:
     ret, frame = capture.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    framehsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     #find_cube_pos(frame)
-    find_cone_pos(frame)
-    cv2.imshow("Frame", frame)
+    find_cone_pos(framehsv)
+    cv2.imshow("Frame", framehsv)
     
     cv2.waitKey(1)
 
